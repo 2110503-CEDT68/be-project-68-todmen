178cf6204f4fb9454178bc413cf552af5cbe43a3
Thitikorn  Worakitpanit <ice@MacBook-ice.local>
2026-03-03 21:19:18 +0700

    ice

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,134 +1,80 @@
+const { text } = require('express');
 const User = require('../models/User');
-//@desc Register user       POST
-//@route                    Public
-//@access                   /api/v1/auth/register
-exports.register = async (req, res, next) => {
-  try {
-    const { name, email, password, role } = req.body;
-
-    // Create user
-    const user = await User.create({
-      name,
-      email,
-      password, // แก้ไขตัวสะกดจาก passwórd
-      role
-    });
-
-    const token=user.getSignedJwtToken();
-
-    /*res.status(200).json({
-      success: true,
-      token
-    });*/
-    sendTokenResponse (user, 200, res);
-
-  } catch (err) {
-    res.status(400).json({
-      success: false
-    });
-    console.log(err.stack);
-  }
+exports.register= async (req, res, next) => {
+    try{
+        const {name,tel, email, password,role}=req.body;
+        console.log(req.body);
+        const user = await User.create({
+            name,
+            tel,
+            email,
+            password,
+            role
+        });
+        sendTokenResponse (user, 200, res) ;
+    }catch(err){
+        res.status(400).json({success:false});
+        console.log(err.stack);
+    }
 };
 
-
-//@desc     Login user
-//@route    POST /api/v1/auth/login
-//@access   Public
-exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-
-    // 1. Validate email & password
+exports.login=async (req, res, next) => { 
+    try{
+    const {email, password, tel}=req.body;
     if (!email || !password) {
-      return res.status(400).json({
-        success: false,
-        msg: 'Please provide an email and password'
-      });
+        return res. status (400).json ({success: false,
+            msg: 'Please provide an email and password' });
     }
+    const user = await User.findOne({email}).select('+password');
 
-    // 2. Check for user (ดึงรหัสผ่านออกมาด้วยเพื่อใช้เทียบ)
-    const user = await User.findOne({ email }).select('+password');
-
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        msg: 'Invalid credentials'
-      });
+    if(!user){
+        return res.status(400).json ({success: false,msg: 'Invalid credentials'});
     }
-
-    // 3. Check if password matches
-    const isMatch = await user.matchPassword(password);
-
+    const isMatch = await user.matchPassword (password);
     if (!isMatch) {
-      return res.status(401).json({
-        success: false,
-        msg: 'Invalid credentials'
-      });
+        return res. status(401).json ({success: false,msg: 'Invalid credentials'});
     }
 
-    // 4. Create token
-    //const token = user.getSignedJwtToken();
-
-    /*res.status(200).json({
-      success: true,
-      token
-    });*/
-    sendTokenResponse(user, 200, res);
-
-
-  } catch (err) {
-    res.status(401).json({
-      success: false,
-      msg: 'Cannot convert email or password to string'
-    });
-  }
+    sendTokenResponse (user, 200, res) ;
+    }catch(err){
+        return res.status(401).json({
+            success: false,
+            msg: 'Cannot convert email or password to string'
+        });
+    }
 };
 
-
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true
-  };
-
-  if (process.env.NODE_ENV === 'production') {
-    options.secure = true;
-  }
-
-  res.status(statusCode)
-    .cookie('token', token, options)
-    .json({
-      success: true,
-      token
-    });
+const sendTokenResponse= (user, statusCode, res) =>{
+    const token=user.getSignedJwtToken () ;
+    const options = {
+        expires: new Date (Date.now()+process. env. JWT_COOKIE_EXPIRE*24*60*60*1000),
+        httponly: true
+    };
+    if (process. env. NODE_ENV==='production' ) {
+        options. secure=true;
+    }
+    res.status(statusCode).cookie ('token', token, options).json ({
+        success: true,
+        token
+    })
+}
+exports.getMe= async (req, res, next) =>{
+    const user=await User. findById(req.user.id) ;
+    res.status(200).json({
+        success: true,data: user
+    })
 };
 
-//@desc     Get current Logged in user
-//@route    GET /api/v1/auth/me
-//@access   Private
-exports.getMe = async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-};
-
-//@desc     Log user out / clear cookie
-//@route    GET /api/v1/auth/logout
-//@access   Private
+// @desc     Log user out / clear cookie
+// @route    GET /api/v1/auth/logout
+// @access   Private
 exports.logout = async (req, res, next) => {
+
   res.cookie('token', 'none', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true
   });
+
   res.status(200).json({
     success: true,
     data: {}
